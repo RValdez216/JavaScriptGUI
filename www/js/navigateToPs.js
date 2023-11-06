89105847dbe4ae03fda2bb6b2ae96487365ee114
Raymond <rvaldez216@mycr.redwoods.edu>
2023-11-05 21:31:30 -0800

    Fixed all red files issue and changed Calculator to portscan gui. Added buttons to take you to and from home screen to port scan gui

---
+++ www/js/navigateToPs.js
@@ -0,0 +1,10 @@
+// Function to navigate to "GUI.html"
+    function navigateToPs() {
+        // Use JavaScript to change the URL to GUI.html
+        console.log("Button clicked");
+        window.location.href = '/www/PortScan.html';
+
+    }
+
+    // Add an event listener to the button
+    document.getElementById("navigateToPs").addEventListener("click", navigateToPs);
